f9120e7c9591108859448fc145c1b3e35d383c41
Konstantinos Vitiniotis <kostasvit@gmail.com>
2024-11-18 10:42:55 +0200

    login view html with email

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,11 +3,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .forms import EmailAuthenticationForm
+from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(
-        template_name='accounts/login.html',
-        authentication_form=EmailAuthenticationForm
-    ), name='login'),
+    path('login/', views.custom_login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import EmailAuthenticationForm
 
-# Create your views here.
+# Login user function
+def custom_login_view(request):
+    if request.method == 'POST':
+        form = EmailAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to home page after successful login
+        else:
+            # If the form is invalid, the error message will be shown on the form
+            return render(request, 'apps/accounts/login.html', {'form': form})
+    else:
+        form = EmailAuthenticationForm()
+    return render(request, 'apps/accounts/login.html', {'form': form})
