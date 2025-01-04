dfba0fb05d288e64f813263a4a7882654486d30e
kostasvit <kostasvit@gmail.com>
2025-01-04 22:25:33 +0200

    profile page changes

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -76,3 +76,11 @@ class UserCreationForm(forms.ModelForm):
         #     for course in courses:
         #         Enrollment.objects.get_or_create(user=user, course=course)
         return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    phone_number = forms.CharField(label='Τηλ. Επικ.',widget=forms.TextInput(attrs={'class':'block w-full rounded-md border-0 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'}),required=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('phone_number',)
