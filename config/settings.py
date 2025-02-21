9f2a342a36b8fc14e017546f7cd944bb701595a2
Konstantinos Vitiniotis <kostasvit@gmail.com>
2025-02-21 08:48:37 +0200

    staticfiles conf

--- config/settings.py
+++ config/settings.py
@@ -163,10 +163,10 @@ LOGOUT_REDIRECT_URL = 'login/'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
+if settings.DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
