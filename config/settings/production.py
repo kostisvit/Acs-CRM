4becaff9c695e33e99e6924666d800b1b07c03de
Konstantinos Vitiniotis <kostasvit@gmail.com>
2026-07-14 09:28:52 +0300

    seperate settings for development-production

---
+++ config/settings/production.py
@@ -0,0 +1,23 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "example.com",
+    "www.example.com",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }
+}
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
