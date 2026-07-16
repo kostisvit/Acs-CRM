d5151ff3ec58189ff26a9731b71e556e5156fcdd
Konstantinos Vitiniotis <kostasvit@gmail.com>
2026-07-16 14:07:10 +0300

    organization app urls

---
+++ apps/organizations/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "organizations"
+
+urlpatterns = [
+    path("", views.organization_list, name="organization_list"),
+]
--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def organization_list(request):
+    return render(request, "organizations/list.html")
