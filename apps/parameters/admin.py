169882e5f30151b8342cf101416e246528390423
Konstantinos Vitiniotis <kostasvit@gmail.com>
2026-07-22 10:21:15 +0300

    parameters apps models

--- apps/parameters/admin.py
+++ apps/parameters/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import JobType, OrgDepartment, TrainingType, TrainingPlace, OtsSoftware
 
-# Register your models here.
+
+@admin.register(JobType)
+class JobTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_active", "created", "modified")
+    list_filter = ("is_active",)
+    search_fields = ("name",)
+  
+@admin.register(OrgDepartment)
+class OrgDepartmentAdmin(admin.ModelAdmin): 
+    list_display = ("name", "is_active", "created", "modified")
+    list_filter = ("is_active",)
+    search_fields = ("name",)
+
+@admin.register(TrainingType)
+class TrainingTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_active", "created", "modified")
+    list_filter = ("is_active",)
+    search_fields = ("name",)
+  
+@admin.register(TrainingPlace)
+class TrainingPlaceAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_active", "created", "modified")
+    list_filter = ("is_active",)
+    search_fields = ("name",)
+    
+@admin.register(OtsSoftware)
+class OtsSoftwareAdmin(admin.ModelAdmin): 
+    list_display = ("name", "is_active", "created", "modified")
+    list_filter = ("is_active",)
+    search_fields = ("name",) 
--- apps/parameters/models.py
+++ apps/parameters/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+class JobType(TimeStampedModel):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Τύπος Εργασίας"
+        verbose_name_plural = "Τύπος Εργασίας"
+
+    def __str__(self):
+        return self.name
+
+
+class OrgDepartment(TimeStampedModel):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Διευθύνσεις Οργανισμού"
+        verbose_name_plural = "Διευθύνσεις Οργανισμού"
+
+    def __str__(self):
+        return self.name
+
+
+class TrainingType(TimeStampedModel):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Εκπαίδευση"
+        verbose_name_plural = "Εκπαίδευση"
+
+    def __str__(self):
+        return self.name
+
+
+class TrainingPlace(TimeStampedModel):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Χώρος Εκπαίδευσης"
+        verbose_name_plural = "Χώρος Εκπαίδευσης"
+
+    def __str__(self):
+        return self.name
+
+
+class AcsAdeia(TimeStampedModel):
+    name = models.CharField(max_length=100, unique=True,
+                            verbose_name="Τύπος Άδειας")
+    adeianame_id = models.CharField(
+        unique=True, verbose_name="ID Τύπου Άδειας", blank=True, null=True
+    )
+    is_active = models.BooleanField(default=True, verbose_name="Ενεργή")
+
+    class Meta:
+        verbose_name = "ACS Τύπος Άδειας"
+        verbose_name_plural = "ACS Τύποι Άδειας"
+
+    def __str__(self):
+        return self.name
+
+
+class OtsSoftware(TimeStampedModel):
+    name = models.CharField(max_length=100, unique=True,
+                            verbose_name="Όνομα Εφαρμογής")
+    is_active = models.BooleanField(default=True, verbose_name="Ενεργή")
+
+    class Meta:
+        verbose_name = "Λογισμικό Οργανισμού"
+        verbose_name_plural = "Λογισμικό Οργανισμού"
+
+    def __str__(self):
+        return self.name
