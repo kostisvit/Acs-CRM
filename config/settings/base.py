a477c2156b01979fb88e6004989e0ccbff9be845
Konstantinos Vitiniotis <kostasvit@gmail.com>
2026-07-22 08:55:47 +0300

    context processor count organizations

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                # Custom context processor for organization, contacts, tasks and day off count
+                "apps.organizations.context_processor.organization_count",
             ],
         },
     },
